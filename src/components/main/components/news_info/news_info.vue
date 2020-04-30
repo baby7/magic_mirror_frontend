48d35a60c78d124105416fa77a1a13954e479436
xiang <1160084407@qq.com>
2020-04-30 13:59:02 +0800

    新闻列表

--- src/components/main/components/news_info/news_info.vue
+++ src/components/main/components/news_info/news_info.vue
@@ -1,20 +1,97 @@
 <template>
-    <h1 class="news-info">北京22日12时到24时新增8例境外输入，全天10例</h1>
+    <div class="box">
+        <p class="news-info">
+            {{news}}
+        </p>
+    </div>
 </template>
 
 <script>
+    import { fetchList } from '@/api/news_info'
+
     export default {
-        name: "NewsInfo"
+        name: "NewsInfo",
+        data() {
+            return {
+                news: "tssssssssssssssssssssssss",
+                timer: '',
+                newsList: [],
+                query: {
+                    current:1,
+                    size:20
+                }
+            }
+        },
+        created() {
+            this.getList()
+        },
+        methods: {
+            getList() {
+                var that = this;
+                fetchList(this.query).then(res => {
+                    if(res.code === 0){
+                        for(let info in res.data.records){
+                            that.newsList.push(res.data.records[info].tilte)
+                        }
+                        that.timer = setInterval(that.change, 10000);
+                    }
+                });
+            },
+            change() {
+                this.news = this.newsList[Math.floor((Math.random()*this.newsList.length))]
+            }
+        },
+        beforeDestroy() {
+            clearInterval(this.timer);
+        }
     }
 </script>
 
 <style scoped>
     .news-info {
-        position:fixed; bottom:0px; left:0px;
+        position:fixed;
+        bottom:0;
+        left:0;
         height: 50px;
-        text-align: center;
+        text-align: right;
         line-height: 50px;
         width: 100%;
         color: #fff;
+        font-size: 40px;
+        display: inline-block;
+        white-space: nowrap;
+        animation: 10s wordsLoop linear infinite normal;
+    }
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    .box {
+        width: 100%;
+        margin: 0 auto;
+        overflow: hidden;
+    }
+
+    @keyframes wordsLoop {
+        0% {
+            transform: translateX(200px);
+            -webkit-transform: translateX(200px);
+        }
+        100% {
+            transform: translateX(-100%);
+            -webkit-transform: translateX(-100%);
+        }
+    }
+
+    @-webkit-keyframes wordsLoop {
+        0% {
+            transform: translateX(200px);
+            -webkit-transform: translateX(200px);
+        }
+        100% {
+            transform: translateX(-100%);
+            -webkit-transform: translateX(-100%);
+        }
     }
 </style>
